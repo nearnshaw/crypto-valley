d9ed78503cf313512a373f597aa68e209d920f3d
nearnshaw <nearnshaw@decentraland.org>
2020-05-14 17:37:25 -0300

    party lights

--- src/partyUpstairs.ts
+++ src/partyUpstairs.ts
@@ -1,6 +1,8 @@
 import utils from '../node_modules/decentraland-ecs-utils/index'
 
 export function startParty() {
+  let partySwitch: boolean = false
+
   const streamSource = new Entity()
   streamSource.addComponent(
     new Transform({ position: new Vector3(56, 10, 55) })
@@ -29,11 +31,13 @@ export function startParty() {
       null, //onTriggerEnter
       null, //onTriggerExit
       () => {
+        partySwitch = true
         musicVideo.playing = true
         music.playing = true
         log('triggered!')
       },
       () => {
+        partySwitch = true
         musicVideo.playing = false
         music.playing = false
         //music.playing = false
@@ -42,6 +46,15 @@ export function startParty() {
     )
   )
   engine.addEntity(MusicTrigger)
+
+  const musicVideo = new VideoTexture(
+    new VideoClip('https://theuniverse.club/live/consensys/index.m3u8')
+  )
+  musicVideo.playing = false
+
+  const musicVideoMaterial = new Material()
+  musicVideoMaterial.albedoTexture = musicVideo
+  musicVideoMaterial.roughness = 1
 
   const flashScreen = new Entity()
   flashScreen.addComponent(new BoxShape())
@@ -52,40 +65,93 @@ export function startParty() {
       scale: new Vector3(5, 5, 5),
     })
   )
-  const musicVideo = new VideoTexture(
-    new VideoClip('https://theuniverse.club/live/consensys/index.m3u8')
-  )
-  musicVideo.playing = false
-
-  const musicVideoMaterial = new Material()
-  musicVideoMaterial.albedoTexture = musicVideo
-  musicVideoMaterial.roughness = 1
   flashScreen.addComponent(musicVideoMaterial)
-
   engine.addEntity(flashScreen)
 
-  //   const ethLogo = new Entity()
-  //   ethLogo.addComponent(new GLTFShape('models/ETH_LOGO.glb'))
-  //   ethLogo.addComponent(
-  //     new Transform({
-  //       position: new Vector3(5, 5, 5),
-  //       rotation: Quaternion.Euler(55, 42, 38.7),
-  //       scale: new Vector3(5, 5, 5),
-  //     })
-  //   )
+  const smallCube1 = new Entity()
+  smallCube1.addComponent(new BoxShape())
+  smallCube1.addComponent(
+    new Transform({
+      position: new Vector3(95.1, 42, 26.73),
+      rotation: Quaternion.Euler(45, 0, 45),
+      scale: new Vector3(3, 3, 3),
+    })
+  )
+  smallCube1.addComponent(musicVideoMaterial)
+  engine.addEntity(smallCube1)
 
-  //   engine.addEntity(ethLogo)
+  const smallCube2 = new Entity()
+  smallCube2.addComponent(new BoxShape())
+  smallCube2.addComponent(
+    new Transform({
+      position: new Vector3(17.2, 42, 26.73),
+      rotation: Quaternion.Euler(45, 0, 45),
+      scale: new Vector3(3, 3, 3),
+    })
+  )
+  smallCube2.addComponent(musicVideoMaterial)
+  engine.addEntity(smallCube2)
+
+  const cone1 = new Entity()
+  cone1.addComponent(new ConeShape())
+  cone1.addComponent(
+    new Transform({
+      position: new Vector3(55, 44, 38.7),
+      rotation: Quaternion.Euler(55, 42, 38.7),
+      scale: new Vector3(3, 3, 3),
+    })
+  )
+  cone1.addComponent(musicVideoMaterial)
+  engine.addEntity(cone1)
+
+  const smallCube3 = new Entity()
+  smallCube3.addComponent(new BoxShape())
+  smallCube3.addComponent(
+    new Transform({
+      position: new Vector3(56.2, 32.5, 21.27),
+      rotation: Quaternion.Euler(45, 0, 45),
+      scale: new Vector3(1, 1, 1),
+    })
+  )
+  smallCube3.addComponent(musicVideoMaterial)
+  engine.addEntity(smallCube3)
 
   class RotatorSystem implements ISystem {
+    cubeRotate: Vector3 = new Vector3(0, 0, 1)
+    coneRotate: Vector3 = new Vector3(1, 0, 1)
     update(dt: number) {
-      //   const ethTransform = ethLogo.getComponent(Transform)
-      //   ethTransform.rotate(Vector3.Up(), dt * 10)
+      if (!partySwitch) {
+        return
+      }
+      const cone1Transform = cone1.getComponent(Transform)
+      cone1Transform.rotate(this.coneRotate, dt * 10)
 
-      const screehTransform = flashScreen.getComponent(Transform)
-      screehTransform.rotate(new Vector3(1, 0, 1), dt * 10)
+      const screenTransform = flashScreen.getComponent(Transform)
+      screenTransform.rotate(this.cubeRotate, dt * 10)
+
+      const small1Transform = smallCube1.getComponent(Transform)
+      small1Transform.rotate(this.cubeRotate, dt * 20)
+
+      const small2Transform = smallCube2.getComponent(Transform)
+      small2Transform.rotate(this.cubeRotate, dt * 20)
+
+      const small3Transform = smallCube3.getComponent(Transform)
+      small3Transform.rotate(this.cubeRotate, dt * 20)
     }
   }
 
   // Add a new instance of the system to the engine
   engine.addSystem(new RotatorSystem())
 }
+
+//   const ethLogo = new Entity()
+//   ethLogo.addComponent(new GLTFShape('models/ETH_LOGO.glb'))
+//   ethLogo.addComponent(
+//     new Transform({
+//       position: new Vector3(5, 5, 5),
+//       rotation: Quaternion.Euler(55, 42, 38.7),
+//       scale: new Vector3(5, 5, 5),
+//     })
+//   )
+
+//   engine.addEntity(ethLogo)
